17632f71d7e39b91e40ae08981a9b12c5254ba6b
kirillspidoznik <kirill.stepanchuk.96@mail.ru>
2020-05-19 20:03:09 +0300

    a lot of commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var write_us_button = document.querySelector(".about-us-adress-button");
+var about_us_adress_button = document.querySelector(".about-us-adress-button");
 var popup_feedback = document.querySelector(".modal-feedback");
 var close_feedback = document.querySelector(".close-feedback");
 var buy_button = document.querySelectorAll(".buy-button");
@@ -9,14 +9,18 @@ var popup_map = document.querySelector(".modal-map");
 var close_map = document.querySelector(".close-map-button");
 var continue_checkout = document.querySelector(".checkout-button-continue");
 
-write_us_button.addEventListener("click", function(evt) {
+if(about_us_adress_button != null){
+about_us_adress_button.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup_feedback.classList.add("modal-show");
 });
+}
+
 close_feedback.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup_feedback.classList.remove("modal-show");
 });
+
 for(let i = 0; i < buy_button.length; i++){
     button = buy_button[i];
     button.addEventListener("click", function(evt){
@@ -24,6 +28,7 @@ for(let i = 0; i < buy_button.length; i++){
         popup_checkout.classList.add("modal-show");
     });
 };
+
 close_checkout.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup_checkout.classList.remove("modal-show");
